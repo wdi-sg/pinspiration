71197ce3a5feacfb2f26f2373d5b1836a64533a2
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-09 00:18:58 +0800

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_scope :user do get "/users/sign_in" => "devise/sessions#new" end
+
+  root 'users#root'
 end

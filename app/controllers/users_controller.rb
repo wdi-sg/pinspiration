f50067e03175ad8fef72ea6feef3b59977289481
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-10 02:26:28 +0800

    Deleted user controller

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-    def root
-      if user_signed_in?
-        render plain: "Logged in!"
-      else
-        redirect_to new_user_session_url
-      end
-    end
-end

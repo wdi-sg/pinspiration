3c79fa2f74009feb768f70eff3648a5d7332db7a
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-09 13:13:41 +0800

    Added routes for pins

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   # devise_scope :user do get "/users/sign_in" => "devise/sessions#new" end
 
   root 'users#root'
+
+  get '/pins' => 'pins#index', as: 'pins'
+  get '/pins/new' => 'pins#new', as: 'new_pin'
+  post '/pins' => 'pins#create'
+  get '/pins/:id' => 'pins#show', as: 'pin'
+  get '/pins/:id/edit' => 'pins#edit', as: 'edit_pin'
+  patch '/pins/:id' => 'pins#update'
+  delete '/pins/:id' => 'pins#destroy'
 end

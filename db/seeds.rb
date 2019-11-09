dc2b9a599b6415b5cad08347bdc6a74ea76add15
SYNG <simyenng@gmail.com>
2019-11-09 18:15:24 +0800

    MVP Utilized ffaker for user generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 5.times do
+#      User.create(
+#         email: FFaker::Internet.email,
+#         password: FFaker::Internet.password,
+#         username: FFaker::Internet.unique.user_name
+#       )
+# end

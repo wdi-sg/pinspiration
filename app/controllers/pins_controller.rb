4deb45f185dbdac2cfc4932af23875b578aa306b
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-10 02:27:45 +0800

    Updated index, create, show and private method to permit form fields

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,14 +1,29 @@
 class PinsController < ApplicationController
+
+    before_action :authenticate_user!
+
     def index
+        if user_signed_in?
+            @pins = Pin.all
+        else
+            redirect_to new_user_session_url
+        end
     end
 
     def new
     end
 
     def create
+        @pin = Pin.new(pin_params)
+
+        @pin.user = current_user
+
+        @pin.save
+        redirect_to @pin
     end
 
     def show
+        @pin = Pin.find(params[:id])
     end
 
     def edit
@@ -19,4 +34,9 @@ class PinsController < ApplicationController
 
     def destroy
     end
+
+    private
+      def pin_params
+        params.require(:pin).permit(:title, :image_url)
+      end
 end

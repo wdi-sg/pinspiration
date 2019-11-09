bb4e4ce7b9444fcfb2fbe443a63179926ac0ace7
SYNG <simyenng@gmail.com>
2019-11-10 00:17:58 +0800

    Add show user route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :pins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pins#index'
+  get '/users/:id' => 'users#show' , as: 'user'
+
 end

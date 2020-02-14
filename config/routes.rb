df05ffa204115c1e0104c1271ab0951c6d6673f4
Robert Kolsek <kolsek.income@gmail.com>
2020-02-14 17:57:11 +0800

    - Made changes to routing to get /dashboard not /dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboards#index'
-  get '/dashboards' => 'pins#index'
+  get '/dashboard' => 'pins#index', as: 'dashboards'
 end

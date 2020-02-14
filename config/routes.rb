eb70f774bc38ae440e2674af82f9271737597e19
Stuart Myers <stuart.myers@gmail.com>
2020-02-14 16:38:36 +0800

    New index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'pins#index'
 end

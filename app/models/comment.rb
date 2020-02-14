75c4a36eff18a34141de6211db42f896674e6960
Stuart Myers <stuart.myers@gmail.com>
2020-02-14 17:05:08 +0800

    Comment user & pin validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :pin
 
   validates :body, presence: true, length: { minimum: 8, maximum: 512 }
-
+  validates :user, presence: true
+  validates :pin, presence: true
 end

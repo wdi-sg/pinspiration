ed87ba573ca30e0e4aaaaba4847049fd3162ec6d
“hweechieh” <hweechieh@gmail.com>
2020-02-14 17:51:57 +0800

    created all tables

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_many :board
+  has_many :comment
+  has_many :pin
+  
+end
---
+++ db/migrate/20200214085936_boards.rb
@@ -0,0 +1,8 @@
+class Boards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :boards do |t|
+      t.string :title
+      t.timestamps
+    end
+  end
+end

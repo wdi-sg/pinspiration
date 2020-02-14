d4b4cf0fea00d12da75539daa25e1078f004b832
upieez <samuelhww@gmail.com>
2020-02-14 15:38:24 +0800

    add routes and pin models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pins#index'
+  get '/pins' => 'pins#index', as: 'pins'
+  get '/pins/new' => 'pins#new', as: 'new_pin'
+  post '/pins' => 'pins#create'
+  get '/pins/:id' => 'pins#show' , as: 'park'
+  get '/pins/:id/edit' => 'pins#edit', as: 'edit_pin'
+  patch '/pins/:id' => 'pins#update'
+  delete '/pins/:id' => 'pins#destroy'
 end

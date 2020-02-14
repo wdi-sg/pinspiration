ca17cb26bd59625e9e2b4b604c4457a59572c8f3
Stuart Myers <stuart.myers@gmail.com>
2020-02-14 17:03:01 +0800

    Added comments controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, length: { minimum: 8, maximum: 512 }
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'comments/show'
+  get 'comments/new'
+  get 'comments/edit'
+  get 'comments/create'
+  get 'comments/update'
+  get 'comments/destroy'
+  
   resources :pins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

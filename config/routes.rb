b73aca188c949ea014dda38a21654de1cc196ad7
Stuart Myers <stuart.myers@gmail.com>
2020-02-15 15:26:44 +0800

    Added a dashboard, it works and everything almost

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'dashboard/index'
   # get 'comments/show'
   # post 'comments/new'
   # get 'comments/edit'
@@ -11,6 +12,6 @@ Rails.application.routes.draw do
   resources :pins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'pins#index'
-  get '/dashboard/' => 'pins#index'
+  get '/' => 'dashboard#index'
+  get '/dashboard/' => 'dashboard#index'
 end

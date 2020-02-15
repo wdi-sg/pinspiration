542021fdc54294b95adf2d529eb56678c8bc6dff
Stuart Myers <stuart.myers@gmail.com>
2020-02-15 16:23:20 +0800

    Using FFaker to add fake users pins and comments

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,5 @@ class Pin < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :img_url, format: {with: /\.(png|jpg)\Z/i}
+  # validates :img_url, format: {with: /\.(png|jpg)\Z/i}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# and have one admin user (me) for testing.
+User.create(email: 'stuart.myers@gmail.com', username: 'StuartAdmin', password: 'MyPassword1234')
+
+# create 20 random users
+20.times do
+  User.create(email: FFaker::Internet.email, username: FFaker::Internet.user_name, password: FFaker::Internet.password)
+end
+
+# create 50 random pins
+50.times do
+  Pin.create(
+    title: FFaker::CheesyLingo.title,
+    img_url: FFaker::Image.url,
+    created_at: FFaker::Time.between(6.months.ago, 1.day.ago),
+    user_id: (rand(20) + 1)
+  )
+end
+
+# create 100 random comments
+100.times do
+  Comment.create(
+    body: FFaker::CheesyLingo.sentence,
+    created_at: FFaker::Time.between(6.months.ago, 1.day.ago),
+    user_id: (rand(20) + 1),
+    pin_id: (rand(49) + 1)
+  )
+end

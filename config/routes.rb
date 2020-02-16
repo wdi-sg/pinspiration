e443d47e6e661501819b02b2a2a3a13cd393afb6
Stuart Myers <stuart.myers@gmail.com>
2020-02-16 17:39:58 +0800

    Can now show tags at /tags/tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'tags/show'
   resources :boards
   resources :comments
   resources :pins
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'dashboard#index'
   get '/dashboard/' => 'dashboard#index'
+  get '/tags/:tag' => 'tags#show', as: 'tag'
 end

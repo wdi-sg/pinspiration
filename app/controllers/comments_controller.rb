24e8bf8ad9b7eec4debe91ed11b675242cc8783a
joycemap <57893220+joycemap@users.noreply.github.com>
2020-02-16 15:55:07 +0800

    Added bootstrap, Comments model - WIP, unpicking in progress

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,85 @@
+class CommentsController < ApplicationController
+  # before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  # GET /comments
+  # GET /comments.json
+  def index
+  end
+
+  # GET /comments/1
+  # GET /comments/1.json
+  def show
+  end
+
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/1/edit
+  def edit
+    @comment = Comment.find(params[:id])
+    if @comment.user == current_user
+      return
+    else
+      redirect_to @comment, notice: 'This comment is not yours.  Can see, cannot touch!'
+    end
+  end
+
+  # POST /comments
+  # POST /comments.json
+  def create
+    @comment = Comment.new(@comment_params)
+    @comment.user == current_user
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment = Comment.find(params[:id])
+    @pin = @comment.pin
+    if @comment.user == current_user
+      @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  else
+    redirect_to @pin, notice: 'Cannot Delete Comment'
+  end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:body, :datetime)
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Pin.create(:title => 'Weird Al Yankovich', :image_url => 'http://i.huffpost.com/gen/1952378/images/o-WEIRD-AL-facebook.jpg'
+)
+Pin.create(:title => 'Kiss', :image_url => 'http://www.gannett-cdn.com/-mm-/b0ad212381eab60e31d1f067f1c478cea741469a/c=0-10-3443-1963&r=x1683&c=3200x1680/local/-/media/USATODAY/GenericImages/2014/03/31//1396298223000-KISS-KISS-BAND-JY-0718-62187918.jpg'
+)
+Pin.create(:title => 'Gwar', :image_url => 'http://www.heavymetal.com/v2/wp-content/uploads/2014/08/gwar_promo.jpg'
+)

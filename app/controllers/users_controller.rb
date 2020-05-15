7c8194bb8082244dbc5c44acddcfc26f953b532b
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-05-16 00:53:49 +0800

    able to update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,24 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @user = User.find(current_user.id)
   end
 
   def edit
+    puts 'hello'
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+
+    @user.update(user_params)
+    redirect_to '/users'
+  end
+
+  private
+    def user_params
+    params.require(:user).permit(:username)
   end
 
 end

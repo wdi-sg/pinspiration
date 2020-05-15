60c12eb68a224a0bd7900405d1df653cce74fecf
Zachariah Chow <zachariahchow@gmail.com>
2020-05-15 17:28:31 +0800

    corrected destroy dependent syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :pins
+  has_many :pins, dependent: :destroy
 end

86077c0f609ac37a047e361c11a89d1df2ff206b
travisenquiry <travisenquiry@gmail.com>
2020-05-16 16:44:59 +0800

    fixed redirect_to of comment & nested routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :pins
+  resources :pins do
+    resources :comments
+  end
 
   root "pins#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

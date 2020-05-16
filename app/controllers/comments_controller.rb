700941f4e8c92fbc29901bc0c593f106a36c9147
osbornechan <osbornecab@gmail.com>
2020-05-17 01:53:52 +0800

    Able to destroy comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -40,6 +40,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to comments_path
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)

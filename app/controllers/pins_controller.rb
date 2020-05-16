5b12c01a34658b112bd0ccb813beee9a33adf48f
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-05-16 19:07:44 +0800

    able to save pin to board

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,25 @@
 class PinsController < ApplicationController
-  def new
 
+  def show
+    @board = current_user.boards.find(params[:board_id])
   end
+
+  def new
+    @board = current_user.boards.find(params[:board_id])
+  end
+
+  def create
+    @board = current_user.boards.find(params[:board_id])
+
+    @pin = @board.pins.create(pin_params)
+    @pin.save
+
+    redirect_to board_pin_url(@board, @pin)
+  end
+
+  private
+    def pin_params
+      params.require(:pin).permit(:title, :image)
+    end
+
 end

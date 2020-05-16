07c5f7238695d0694aa0023105a3e6b7533afb44
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-05-16 16:19:31 +0800

    created association with board and users

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,22 @@
 class BoardsController < ApplicationController
+  def show
+    @board = Board.find(params[:id])
+  end
+
   def new
   end
+
+  def create
+    @current_user = current_user
+    @board = @current_user.boards.create(board_params)
+
+    @board.save
+    redirect_to @board
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:name)
+    end
+
 end

11aaad6ccdea2d85f3afff9c528b7ec491b44a58
mdazharaw <darkaz62@hotmail.com>
2020-05-17 12:03:40 +0800

    Changed route to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  resources :pins
+  resources :pins 
+  # do  
+  #   resources :comments, :except => [:update , :destroy, :show]
+  #   # resources :comments, :only => [:post]
+  # end
+  # resources :comments, :only => [:update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pins#index'
+  root :to => redirect('/dashboard')
+
+  get '/dashboard' => 'pins#index' , as: 'dashboard'
+
+  # get '/pins/:pin_id/comments/new' => 'comments#create', as: 'new_pin_comment'
+  # post '/pins/:pin_id/comments' => 'comments#create'
+  # put '/pins/:pin_id/comments/:id' => 'comments#create'
+  # delete '/pins/:pin_id/comments/:id' => 'comments#create'
+
 end

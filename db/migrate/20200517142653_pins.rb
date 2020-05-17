9a33677d222b53daeef6c4239113698830fcea4d
vigbit <vigneshb@live.com>
2020-05-17 22:36:05 +0800

    "initial commit"

---
+++ db/migrate/20200517142653_pins.rb
@@ -0,0 +1,11 @@
+class Pins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pins do |t|
+      t.string :title
+      t.string :description
+      t.string :url
+      t.references :user
+      t.timestamps
+    end
+  end
+end

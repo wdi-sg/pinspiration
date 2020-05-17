9c5fd920765e5b912586b6e2c9430b09c7605927
Rachelik <rachel.ik.87@gmail.com>
2020-05-17 23:49:18 +0800

    edit some css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+one = Pin.new(title: "Staggered tall buildings", photo_url: "https://i.picsum.photos/id/221/350/350.jpg" caption: "looks good" user_id: 1)
+one.save
+
+two = Pin.new(title: "Typewriter parts", photo_url: "https://i.picsum.photos/id/252/500/500.jpg" caption: "I think so." user_id: 1)
+two.save
+
+three = Pin.new(title: "Library", photo_url: "https://i.picsum.photos/id/444/400/600.jpg" caption: "Cannot finish reading all those books even if I stay there." user_id: 1)
+three.save
+
+four = Pin.new(title: "Ruler", photo_url: "https://i.picsum.photos/id/533/350/350.jpg" caption: "Rulers always rules. Especially folded ones." user_id: 1)
+four.save
+
+five = Pin.new(title: "Hot-air Balloons", photo_url: "https://i.picsum.photos/id/533/400/400.jpg" caption: "High up in the air." user_id: 1)
+five.save

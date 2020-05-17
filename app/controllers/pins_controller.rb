a01d7721ff997a28714a14ce65485c23046c508a
qundax <qunda.lim@outlook.sg>
2020-05-17 13:03:28 +0800

    Added Pinx#Index, and Pins#New views; Updated PinsController

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   def index
+    @pins = Pin.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pins#index'
+
+  get '/' => 'pins#index'
+  get '/pins/new' => 'pins#new', as: 'new_pin'
+  get '/pins/:id' => 'pins#show', as: 'pin'
+
+  post '/pins' => 'pins#create'
 end

cb9b207ba2c7b3fb7b40e5222e3630953c07f4fd
lekhweemeng <lek.hweemeng@live.com>
2020-05-18 00:17:47 +0800

    added in create function for pins and completed user ability to sign in

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,18 +1,26 @@
 class PinsController < ApplicationController
   def index
-    # @pins = Pin.all
+    @pins = Pin.all
   end
 
   def show
+    @pin = Pin.find(params[:id])
   end
 
   def new
+    # @pin = Pin.new
   end
 
   def edit
   end
 
   def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+    redirect_to @pin
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -20,4 +28,9 @@ class PinsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def pin_params
+      params.require(:pin).permit(:title, :image_url, :comment, :user_id)
+    end
 end

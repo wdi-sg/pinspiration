2c392e1a7900e06a84f988c7d29ff077323522bf
kokchuantan <kokchuan1996@gmail.com>
2020-05-17 15:10:03 +0800

    added routes/functions for pins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pins#index'
+  get '/pins' => 'pins#index', as: 'pins'
+  get '/pins/new' => 'pins#new', as: 'new_pin'
+  post '/pins' => 'pins#create'
+  get '/pins/:id' => 'pins#show' , as: 'pin'
+  get '/pins/:id/edit' => 'pins#edit', as: 'edit_pin'
+  patch '/pins/:id' => 'pins#update'
+  delete '/pins/:id' => 'pins#destroy'
+
 end

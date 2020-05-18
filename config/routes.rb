37878057e38fa8e7aa024bcb3e96098d970b50d3
mdazharaw <darkaz62@hotmail.com>
2020-05-18 14:33:53 +0800

    Change page name to pinspiration

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => redirect('/dashboard')
 
